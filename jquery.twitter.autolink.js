1bb8ef1484f99a103bc78885abfb50b0039b2873
Graham Swan <thinkswan@gmail.com>
2013-09-19 22:25:08 -0600

    commented code

--- jquery.twitter.autolink.js
+++ jquery.twitter.autolink.js
@@ -1,3 +1,14 @@
+/*
+  jQuery Twitter Autolink - v1.0
+  
+  Automatically creates links for URLs, email addresses, @-mentions, and hashtags in embedded tweets.
+  
+  Usage: $(<tweet_element>).twitter_autolink();
+  
+  Copyright (c) 2013 Graham Swan
+  Licensed under the MIT license - https://github.com/thinkswan/jquery-twitter-autolink/blob/master/LICENSE
+*/
+
 (function($) {
   $.fn.twitter_autolink_constants = {
     TWITTER_BASE_URL: 'http://twitter.com/',
@@ -5,19 +16,19 @@
   }
   $.fn.twitter_autolink_regex_map = new Array(
     {
-      're': /((http|https|ftp):\/\/[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>))/g,
+      're': /((http|https|ftp):\/\/[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>))/g, // URLs
       'replace': '<a href="$1">$1</a>'
     },
     {
-      're': /(([a-z0-9*._+]){1,}\@(([a-z0-9]+[-]?){1,}[a-z0-9]+\.){1,}([a-z]{2,4}|museum)(?![\w\s?&.\/;#~%"=-]*>))/g,
+      're': /(([a-z0-9*._+]){1,}\@(([a-z0-9]+[-]?){1,}[a-z0-9]+\.){1,}([a-z]{2,4}|museum)(?![\w\s?&.\/;#~%"=-]*>))/g, // Email addresses
       'replace': '<a href="mailto:$1">$1</a>'
     },
     {
-      're': /(^|\s)@(\w+)/g,
+      're': /(^|\s)@(\w+)/g, // @-mentions
       'replace': '$1<a href="' + $.fn.twitter_autolink_constants.TWITTER_BASE_URL + '$2">@$2</a>'
     },
     {
-      're': /(^|\s)#(\w+)/g,
+      're': /(^|\s)#(\w+)/g, // Hashtags
       'replace': '$1<a href="' + $.fn.twitter_autolink_constants.TWITTER_SEARCH_URL + '%23$2">#$2</a>'
     }
   );
